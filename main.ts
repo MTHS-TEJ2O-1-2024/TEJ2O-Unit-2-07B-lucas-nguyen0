8d7aad007b1d13360111aa492e8df1eccffe709e
lucas-nguyen0 <lucas.nguyen@mths.ca>
2024-10-01 10:59:29 -0400

    micro:bit now can play Cookie Clicker

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Lucas Nguyen
+ * Created on: Oct 2024
+ * This program plays Cookie Clicker
 */
 
-basic.showString('Hello, World!')
+let cookieNumber: number
+
+basic.clearScreen()
+
+cookieNumber = 0
+basic.showString(cookieNumber.toString())
+
+input.onButtonPressed(Button.A, function () {
+    cookieNumber = cookieNumber + 1
+    basic.showString(cookieNumber.toString())
+})
+
+input.onButtonPressed(Button.B, function () {
+    cookieNumber = 0
+    basic.showString(cookieNumber.toString())
+})
